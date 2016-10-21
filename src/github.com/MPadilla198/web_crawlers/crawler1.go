c12e23c37f89e20e861840d1ee7e6c82f9e25c4f
MPadilla198 <miguel.padilla.harmony2013@gmail.com>
2016-10-20 20:30:25 -0500

    crawler1: move href code into it's own function and reworked internal mechs

--- src/github.com/MPadilla198/web_crawlers/crawler1.go
+++ src/github.com/MPadilla198/web_crawlers/crawler1.go
@@ -1,12 +1,15 @@
 package main
 
+// https://schier.co/blog/2015/04/26/a-simple-web-scraper-in-go.html
+// Web scraper from above tutorial
+
 import (
   "net/http"
   //"io/ioutil"
   "golang.org/x/net/html"
   "fmt"
   //"os"
-  //"strings"
+  "strings"
 )
 
 func main() {
@@ -18,21 +21,30 @@ func main() {
   // Change url to scrape different websites
   url := "https://www.golang.org"
 
-  resp, _ := http.Get(url)
-  //bytes, _ := ioutil.ReadAll(resp.Body)
-
-  //fmt.Println("HTML:\n\n", string(bytes))
-
-  parseHTML(resp)
-
-  resp.Body.Close()
+  Crawl(url)
 }
 
-func parseHTML(resp *http.Response) {
+// Function to find href attribute from a Token
+func GetHref(t html.Token) (ok bool, href string) {
+  //Iterates over the attributes in t to find "href"
+  for _, a := range t.Attr {
+    if a.Key == "href" {
+      href = a.Val
+      ok = true
+      return
+    }
+  }
+
+  return
+}
+
+func Crawl(url string) {
 
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
   // Parse HTML for Anchor Tags
   //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+  resp, _ := http.Get(url)
 
   htmlTokenizer := html.NewTokenizer(resp.Body)
 
@@ -46,15 +58,25 @@ func parseHTML(resp *http.Response) {
     case tt == html.StartTagToken:
       t := htmlTokenizer.Token()
 
+      // Finds a tags in html
       isAnchor := t.Data == "a"
-      if isAnchor {
-        for _, a := range t.Attr {
-          if a.Key == "href" {
-            fmt.Println("We found a link:", a.Val)
-            break
-          }
-        }
+      if !isAnchor {
+        continue
+      }
+
+      // Get href attribute from t if present
+      ok, val := GetHref(t)
+      if !ok {
+        continue
+      }
+
+      // Makes sure the url begins with "http"
+      hasProto := strings.Index(val, "http") == 0
+      if hasProto {
+        fmt.Println("Found a link:", val)
       }
     }
   }
+
+  resp.Body.Close()
 }
