eb783d2e055d118f73c02d6ab0e1c38a54354535
Miguel Padilla <miguel.padilla.harmony2013@gmail.com>
2016-12-08 10:49:47 -0600

    Update queue.go
    Added Clear, Has, IndexOf, IsEmpty, Peek, Pop, Push functions for Queue

--- src/github.com/MPadilla198/datastructures/collections/queue.go
+++ src/github.com/MPadilla198/datastructures/collections/queue.go
@@ -1,1 +1,43 @@
 package collections
+
+type Queue struct {
+	list *linkedList
+}
+
+func (queue *Queue) Clear() {
+	
+	queue.list = new(linkedList)
+}
+
+func (queue *Queue) Has(val int) bool {
+	
+	return queue.list.Has(val)
+}
+
+func (queue *Queue) IndexOf(val int) bool {
+	
+	return queue.list.IndexOf(val)
+}
+
+func (queue *Queue) IsEmpty() bool {
+	
+	return queue.list.IsEmpty()
+}
+
+func (queue *Queue) Peek() int {
+	
+	return queue.list.rootNode.value
+}
+
+func (queue *Queue) Pop() int {
+	
+	val = queue.list.Peek()
+	queue.list.Remove(val)
+	
+	return val
+}
+
+func (queue *Queue) Push(val int) int {
+	
+	queue.list.AddToTail(val)
+}
