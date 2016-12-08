5d988848bb8f600dc7a402f4dc1d63b2ecda5f65
Miguel Padilla <miguel.padilla.harmony2013@gmail.com>
2016-12-08 10:21:05 -0600

    Update stack.go
    Added Empty, Has, IndexOf, Peek, Pop, Push functions in Stack

--- src/github.com/MPadilla198/datastructures/collections/stack.go
+++ src/github.com/MPadilla198/datastructures/collections/stack.go
@@ -1,1 +1,38 @@
 package collections
+
+type Stack struct {
+  list *linkedList
+}
+
+func (stack *Stack) Empty() bool {
+	
+	return stack.list.Empty()
+}
+
+func (stack *Stack) Has(val int) bool {
+	
+	return stack.list.Has(val)
+}
+
+func (stack *Stack) IndexOf(val int) int {
+	
+	return stack.list.IndexOf(val)
+}
+
+func (stack *Stack) Peek() int {
+	
+	return stack.list.Peek()
+}
+
+func (stack *Stack) Pop() int {
+  
+	val = stack.list.Peek()
+	stack.list.Remove(val)
+	
+	return val
+}
+
+func (stack *Stack) Push(val int) {
+  
+  stack.list.AddToHead(val)
+}
